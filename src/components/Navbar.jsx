1b34e2603cf152cc8c303d01a420a0fa70c24b0f
chorongs <charmblues@gmail.com>
2023-03-06 21:15:42 +0900

    feat: user profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { login, logout, onUserStateChange } from '../api/firebase';
+import User from './User';
 
 export default function Navbar() {
   const [user, setUser] = useState();
@@ -32,6 +33,7 @@ export default function Navbar() {
         <Link to='rooms/new' className='text-2xl'>
           <BsFillPencilFill />
         </Link>
+        {user && <User user={user} />}
         {!user && <button onClick={handleLogin}>Login</button>}
         {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
